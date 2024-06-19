1ff861cac7cc5aef844dacdc0f7e57ad55903160
mitraaa1 <mitrabasi@hotmail.com>
2024-06-19 21:02:20 +0200

    TernarySearch from Mitra

--- src/main/java/com/williamfiset/algorithms/search/TernarySearchCoverageTool.java
+++ src/main/java/com/williamfiset/algorithms/search/TernarySearchCoverageTool.java
@@ -3,25 +3,25 @@ package com.williamfiset.algorithms.search;
 public class TernarySearchCoverageTool {
 
     // Coverage tracking
-    private static boolean branch1Hit = false;
-    private static boolean branch2Hit = false;
-    private static boolean loopBreakHit = false;
+    private static boolean branch101Hit = false;
+    private static boolean branch102Hit = false;
+    private static boolean branch103Hit = false;
 
-    public static void trackBranch1Hit() {
-        branch1Hit = true;
+    public static void trackBranch101Hit() {
+        branch101Hit = true;
     }
 
-    public static void trackBranch2Hit() {
-        branch2Hit = true;
+    public static void trackBranch102Hit() {
+        branch102Hit = true;
     }
 
-    public static void trackLoopBreakHit() {
-        loopBreakHit = true;
+    public static void trackBranch103Hit() {
+        branch103Hit = true;
     }
 
     public static void printCoverage() {
-        System.out.println("Branch 1 (res1 > res2) was " + (branch1Hit ? "hit" : "not hit"));
-        System.out.println("Branch 2 (res1 <= res2) was " + (branch2Hit ? "hit" : "not hit"));
-        System.out.println("Loop break was " + (loopBreakHit ? "hit" : "not hit"));
+        System.out.println("Branch 1 (res1 > res2) was " + (branch101Hit ? "hit" : "not hit"));
+        System.out.println("Branch 2 (res1 <= res2) was " + (branch102Hit ? "hit" : "not hit"));
+        System.out.println("Loop break was " + (branch103Hit? "hit" : "not hit"));
     }
 }
